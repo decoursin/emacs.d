b4fcea2035bdb90952fdbe686882cc234ceb22e9
Nick <ndecoursin@gmail.com>
2016-04-22 22:23:04 -0500

    custom.el should this really be tracked?

--- custom.el
+++ custom.el
@@ -3,9 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(hcl-indent-level 4)
  '(package-selected-packages
    (quote
-    (evil-nerd-commenter evil-anzu evil-terminal-cursor-changer evil-surround evil-visualstar fullframe exec-path-from-shell evil-tabs evil-leader evil-jumper))))
+    (json-reformat-region zenburn-theme zeal-at-point yaml-mode yagist whitespace-cleanup-mode wgrep-ag web-mode tidy terraform-mode tagedit sql-indent skewer-less scss-mode sass-mode regex-tool react-snippets rainbow-mode rainbow-delimiters project-local-variables pip-requirements pcomplete-extension paradox page-break-lines mwe-log-commands mmm-mode matlab-mode markdown-mode magit-gh-pulls macrostep less-css-mode latest-clojure-libraries jsx-mode json-mode js-comint ipretty ibuffer-vc hl-sexp highlight-symbol highlight-quoted highlight-escape-sequences helm-projectile guide-key gitignore-mode github-clone github-browse-file gitconfig-mode git-timemachine git-messenger git-blame fullframe flycheck-tip flycheck-package flycheck-elm flycheck-clojure fill-column-indicator expand-region exec-path-from-shell evil-visualstar evil-terminal-cursor-changer evil-tabs evil-surround evil-smartparens evil-nerd-commenter evil-magit evil-leader evil-jumper evil-anzu eval-sexp-fu elm-mode elein dockerfile-mode dired-sort dired+ diminish diff-hl css-eldoc corral company coffee-mode clojure-cheatsheet cljsbuild-mode clj-refactor cl-lib-highlight bug-reference-github browse-kill-ring auto-compile ag adjust-parens ace-jump-helm-line ac-js2 4clojure)))
+ '(terraform-indent-level 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
