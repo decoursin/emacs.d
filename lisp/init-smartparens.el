7068eaad5485e106d1f17b1628b788a22ca9c02f
Nick DeCoursin <ndecoursin@gmail.com>
2017-11-13 20:16:48 +0100

    smartparens global

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -1,6 +1,7 @@
 ;; Created by Nick DeCoursin
 
 
+(require-package 'smartparens)
 (require-package 'evil-smartparens)
 
 (require 'evil-smartparens)
@@ -13,9 +14,14 @@
 (setq sp-navigate-reindent-after-up nil)
 (setq sp-navigate-close-if-unbalanced nil)
 
-;(add-hook 'lisp-interaction-mode-hook #'evil-smartparens-mode); (deprecated)
-(add-hook 'emacs-lisp-mode-hook #'evil-smartparens-mode)
-(add-hook 'cider-mode-hook #'evil-smartparens-mode)
-(add-hook 'clojure-mode-hook #'evil-smartparens-mode)
+;; (add-hook 'lisp-interaction-mode-hook #'evil-smartparens-mode); (deprecated)
+
+;; (add-hook 'emacs-lisp-mode-hook #'evil-smartparens-mode)
+;; (add-hook 'cider-mode-hook #'evil-smartparens-mode)
+;; (add-hook 'clojure-mode-hook #'evil-smartparens-mode)
+;; (add-hook 'clojurescript-mode-mode-hook #'evil-smartparens-mode)
+
+(show-smartparens-global-mode t)
+(add-hook 'smartparens-enabled-hook #'evil-smartparens-mode)
 
 (provide 'init-smartparens)
