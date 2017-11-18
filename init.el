c70d29fd954d082d5d2872c78f2a62aefa1e455e
Nick DeCoursin <ndecoursin@gmail.com>
2017-11-18 09:10:34 +0100

    remove min emacs version check

--- init.el
+++ init.el
@@ -7,12 +7,6 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(let ((minver "23.3"))
-  (when (version<= emacs-version "23.1")
-    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
-(when (version<= emacs-version "24")
-  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
-
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'init-benchmarking)
 
