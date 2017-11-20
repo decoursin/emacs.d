882859ffefd5e4fdd027b336563a039996fa207d
Nick DeCoursin <ndecoursin@gmail.com>
2017-11-20 21:41:06 +0100

    company new default keybinds

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -7,5 +7,15 @@
 (setq company-idle-delay nil) ; never start completions automatically
 
 
+(after-load 'company
+  (define-key company-active-map (kbd "j") 'company-select-next)
+  (define-key company-active-map (kbd "k") 'company-select-previous)
+  (define-key company-active-map (kbd "SPC") 'company-complete-selection)
+  (define-key company-active-map (kbd "C-SPC") 'company-select-next)
+  (define-key company-active-map (kbd "S-C-SPC") 'company-select-previous)
+  ;; I don't know what these do/are.
+  (setq-default company-backends '((company-capf company-dabbrev-code) company-dabbrev)
+                company-dabbrev-other-buffers 'all
+                company-tooltip-align-annotations t))
 
 (provide 'init-company)
