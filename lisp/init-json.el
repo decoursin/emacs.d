ebad2a95d6784e07005246fb6dbd065a3d4f7f94
decoursin <ndecoursin@gmail.com>
2018-01-27 21:56:40 +0100

    json file types

--- lisp/init-json.el
+++ lisp/init-json.el
@@ -3,5 +3,8 @@
 
 (require-package 'flymake-json)
 
+(add-to-list 'auto-mode-alist '("\\.eslintrc.*$" . json-mode))
+(add-to-list 'auto-mode-alist '("\\.babelrc$" . json-mode))
+
 ;; Untested yet
 (add-hook 'json-mode 'flymake-json-load)
