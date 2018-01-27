ff8ac7abc62299d1ea2f48988b781710b4ced202
decoursin <ndecoursin@gmail.com>
2018-01-27 21:55:52 +0100

    flycheck, js changes, oops what were these changes?
    This should have been committed or removed, now
    these are blind commints

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -7,11 +7,12 @@
 (use-package flycheck-clojure)
 (use-package flycheck-pos-tip)
 (use-package flycheck-tip)
+(use-package flycheck-flow)
 
 ;;; enable clojure cider checkers
-;; (after-load 'cider
-;;   (after-load 'flycheck
-;;     (flycheck-clojure-setup)))
+(after-load 'cider
+  (after-load 'flycheck
+    (flycheck-clojure-setup)))
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
@@ -22,21 +23,67 @@
 (setq flycheck-navigation-minimum-level 'errors)
 
 ;;; frequency
-(setq flycheck-check-syntax-automatically '(mode-enabled save idle-change))
-;; (setq flycheck-idle-change-delay 0.8)
+(setq flycheck-check-syntax-automatically '(mode-enabled save))
+;; (setq flycheck-idle-change-delay 1.)
 
 ;;; disable checkers
 (with-eval-after-load 'flycheck
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc
                                              ;; clojure-cider-kibit
                                              ;; clojure-cider-eastwood
-                                             ;; clojure-cider-typed
+                                             clojure-cider-typed
+                                             ;; clojure-joker
+                                             javascript-jshint
+                                             ;; javascript-jscs
+                                             ;; javascript-gjslint
                                              )))
+
+;;;;;;;;;;;;;;;;;;;;;; javascript
+
+(flycheck-add-mode 'javascript-eslint 'rjsx-mode)
+
+;; use local eslint from node_modules before global
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+(defun my/use-flow-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (flow (and root
+                    (expand-file-name "node_modules/flow-bin/vendor/flow"
+                                      root))))
+    (when (and flow (file-executable-p flow))
+      (setq-local flycheck-javascript-flow-executable flow))))
+
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+(add-hook 'flycheck-mode-hook #'my/use-flow-from-node-modules)
+
+;; Flycheck + Flowtype
+(require 'flycheck-flow)
+(flycheck-add-next-checker 'javascript-flow 'javascript-eslint)
+
+;;;;;;;;;;;;;;;;;;;;; end JS
+
+;; customize flycheck temp file prefix
+(setq-default flycheck-temp-prefix ".flycheck")
 
 ;; Make flycheck recognize packages in loadpath
 ;; i.e (require 'company) will not give an error now
 (setq flycheck-emacs-lisp-load-path 'inherit)
 
+(eval-after-load 'flycheck
+  '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
 
 ;; (eval-after-load 'flycheck '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
 
--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -1,134 +1,72 @@
-;; Nick, here's a new
-;; guide: https://github.com/xiaohanyu/oh-my-emacs/blob/master/modules/ome-javascript.org
-;; another guide: https://yoo2080.wordpress.com/2012/03/15/js2-mode-setup-recommendation/
-;; Copied from Purcell
-;; Untested
-;; IDK much about this file
-
 (maybe-require-package 'json-mode)
 (maybe-require-package 'js2-mode)
 (maybe-require-package 'json-reformat-region)
-(require-package 'js-comint)
 (require-package 'react-snippets)
 (require-package 'rjsx-mode)
 
-(defcustom preferred-javascript-mode
-  (first (remove-if-not #'fboundp '(rjsx-mode js2-mode js-mode)))
-  "Javascript mode to use for .js files."
-  :type 'symbol
-  :group 'programming
-  :options '(rjsx-mode js2-jsx-mode js2-mode js-mode))
+;; javascript / html
+(add-to-list 'auto-mode-alist '("\\.js$" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("\\.eslintrc.*$" . json-mode))
+(add-to-list 'auto-mode-alist '("\\.babelrc$" . json-mode))
 
 ;; indent
 (defconst preferred-javascript-indent-level 4)
 (setq json-reformat:indent-width 4)
 (setq js2-basic-offset 4)
+(setq js-indent-level 4)
 
-;; js2-jsx-mode
-;; (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
-;; (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
-
-;;; TODO: try this Nick
-;;; Link: https://github.com/syl20bnr/spacemacs/blob/a298a5a311dfe8b449b5f538740303f1ab3d5695/layers/%2Bframeworks/react/packages.el
-;; (defun spacemacs//setup-react-mode ()
-;;   "Adjust web-mode to accommodate react-mode"
-;;   (emmet-mode 0)
-;;   ;; See https://github.com/CestDiego/emmet-mode/commit/3f2904196e856d31b9c95794d2682c4c7365db23
-;;   (setq-local emmet-expand-jsx-className? t)
-;;   ;; Enable js-mode snippets
-;;   (yas-activate-extra-mode 'js-mode)
-;;   ;; Force jsx content type
-;;   (web-mode-set-content-type "jsx")
-;;   ;; Don't auto-quote attribute values
-;;   (setq-local web-mode-enable-auto-quoting nil)
-;;   ;; Why do we do this ?
-;;   (defadvice web-mode-highlight-part (around tweak-jsx activate)
-;;     (let ((web-mode-enable-part-face nil))
-;;       ad-do-it)))
-;; 
-;; (add-hook 'js2-jsx-mode-hook 'spacemacs//setup-react-mode)
-
-(defun setup-js2-mode-syntax-checker ()
-  (flycheck-select-checker 'javascript-eslint)
-  (flycheck-mode))
-
-;; Need to first remove from list if present, since elpa adds entries too, which
-;; may be in an arbitrary order
-(eval-when-compile (require 'cl))
-(setq auto-mode-alist (cons `("\\.\\(js\\|es6\\)\\(\\.erb\\)?\\'" . ,preferred-javascript-mode)
-                            (loop for entry in auto-mode-alist
-                                  unless (eq preferred-javascript-mode (cdr entry))
-                                  collect entry)))
-
-
-;; js2-mode
-
-;; Change some defaults: customize them to override
-(setq-default js2-bounce-indent-p nil)
-(after-load 'js2-mode
-  ;; js2 mode's syntax error highlighting defaults
-  (setq-default js2-mode-show-parse-errors t
-                js2-mode-show-strict-warnings t)
-  (setq-default js2-strict-trailing-comma-warning nil)
-  ;; ... but enable it if flycheck can't handle javascript
-  (autoload 'flycheck-get-checker-for-buffer "flycheck")
-  ;;;; deprecate by Nick
-  ;; (defun sanityinc/disable-js2-checks-if-flycheck-active ()
-  ;;   (unless (flycheck-get-checker-for-buffer)
-  ;;     (set (make-local-variable 'js2-mode-show-parse-errors) t)
-  ;;     (set (make-local-variable 'js2-mode-show-strict-warnings) t)))
-  ;; (add-hook 'js2-mode-hook 'sanityinc/disable-js2-checks-if-flycheck-active)
-
-  (add-hook 'js2-mode-hook (lambda () (setq mode-name "JS2")))
-
-  (add-hook 'js2-mode-hook #'setup-js2-mode-syntax-checker)
-
-  (after-load 'js2-mode
-    (js2-imenu-extras-setup)))
-
-;; js-mode
-(setq-default js-indent-level preferred-javascript-indent-level)
-
-
-(add-to-list 'interpreter-mode-alist (cons "node" preferred-javascript-mode))
-
-
-;; Javascript nests {} and () a lot, so I find this helpful
+(eval-after-load "sgml-mode"
+  '(progn
+     (require 'tagedit)
+     (tagedit-add-paredit-like-keybindings)
+     (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
 
 (require-package 'rainbow-delimiters)
 (dolist (hook '(js2-mode-hook js-mode-hook json-mode-hook))
   (add-hook hook 'rainbow-delimiters-mode))
 
+;; coffeescript
+(add-to-list 'auto-mode-alist '("\\.coffee.erb$" . coffee-mode))
+(add-hook 'coffee-mode-hook 'subword-mode)
+(add-hook 'coffee-mode-hook 'highlight-indentation-current-column-mode)
+(add-hook 'coffee-mode-hook
+          (defun coffee-mode-newline-and-indent ()
+            (define-key coffee-mode-map "\C-j" 'coffee-newline-and-indent)
+            (setq coffee-cleanup-whitespace nil)))
+(custom-set-variables
+ '(coffee-tab-width 2))
 
-;; ---------------------------------------------------------------------------
-;; Run and interact with an inferior JS via js-comint.el
-;; ---------------------------------------------------------------------------
+;;;;;;;;;;;;;;;;;;;;;; setup-prettier.el
 
-(setq inferior-js-program-command "js")
+(require-package 'prettier-js)
 
-(defvar inferior-js-minor-mode-map (make-sparse-keymap))
-(define-key inferior-js-minor-mode-map "\C-x\C-e" 'js-send-last-sexp)
-(define-key inferior-js-minor-mode-map "\C-\M-x" 'js-send-last-sexp-and-go)
-(define-key inferior-js-minor-mode-map "\C-cb" 'js-send-buffer)
-(define-key inferior-js-minor-mode-map "\C-c\C-b" 'js-send-buffer-and-go)
-(define-key inferior-js-minor-mode-map "\C-cl" 'js-load-file-and-go)
+(setq prettier-js-width-mode 'fill)
+(setq prettier-js-args '("--single-quote" "--trailing-comma=all"))
 
-(define-minor-mode inferior-js-keys-mode
-  "Bindings for communicating with an inferior js interpreter."
-  nil " InfJS" inferior-js-minor-mode-map)
+(defun my/use-prettier-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (prettier (and root
+                        (expand-file-name "node_modules/.bin/prettier"
+                                          root))))
+    (when (and prettier (file-executable-p prettier))
+      (setq-local prettier-js-command prettier))))
 
-(dolist (hook '(js2-mode-hook js-mode-hook))
-  (add-hook hook 'inferior-js-keys-mode))
+(add-hook 'js-mode-hook 'prettier-js-mode)
+(add-hook 'js-mode-hook #'my/use-prettier-from-node-modules)
+(add-hook 'js2-mode-hook 'prettier-js-mode)
+(add-hook 'js2-mode-hook #'my/use-prettier-from-node-modules)
+(add-hook 'web-mode-hook 'prettier-js-mode)
+(add-hook 'web-mode-hook #'my/use-prettier-from-node-modules)
+;; CSS
+(add-hook 'css-mode-hook 'prettier-js-mode)
+(add-hook 'css-mode-hook #'my/use-prettier-from-node-modules)
+(add-hook 'scss-mode-hook 'prettier-js-mode)
+(add-hook 'scss-mode-hook #'my/use-prettier-from-node-modules)
 
-;; ---------------------------------------------------------------------------
-;; Alternatively, use skewer-mode
-;; ---------------------------------------------------------------------------
+;; (provide 'setup-prettier)
 
-(when (maybe-require-package 'skewer-mode)
-  (after-load 'skewer-mode
-    (add-hook 'skewer-mode-hook
-              (lambda () (inferior-js-keys-mode -1)))))
-
+;;;;;;;;;;;;;;;;;;;;;; setup-prettier.el ends here
 
 (provide 'init-javascript)
