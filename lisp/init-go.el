809ceff6112f3387f98581f522d31dcebf11b20c
decoursin <ndecoursin@gmail.com>
2018-01-29 12:49:34 +0100

    init-go changes

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -2,15 +2,15 @@
 (require-package 'go-mode)
 (require-package 'go-eldoc)
 (require-package 'go-autocomplete)
-(require-package 'go-flycheck)
 
 (add-hook 'go-mode-hook 'go-eldoc-setup)
 
 ;; learn: https://johnsogg.github.io/emacs-golang.
 
+;; doesn't work..
 ;;; I don't know what this is.
 ;; highlight identifiers
-(go-guru-hl-identifier-mode)
+;;(go-guru-hl-identifier-mode)
 
 ;; goimports, which organizes the imports at the top of each file. 
 (setq gofmt-command "goimports")
