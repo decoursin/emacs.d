724e82161bd988738aa4076a28ea7b1fff44e678
decoursin <ndecoursin@gmail.com>
2018-03-01 13:48:36 +0100

    python auto-completion

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -7,12 +7,19 @@
 
 (require-package 'pip-requirements)
 
+(require-package 'company-jedi)
+
 ;; following was created by me
 (require-package 'python-mode)
 
 (autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+
+(setq flycheck-python-pycompile-executable "python3")
+
+;; for autocompletion
+;; (add-hook 'python-mode-hook (lambda () (add-to-list 'company-backends 'company-jedi)))
 
 
 (provide 'init-python-mode)
