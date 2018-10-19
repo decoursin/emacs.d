dab8663437be512343fdd45b8b5ce2156e47a546
decoursin <ndecoursin@gmail.com>
2018-10-19 07:58:27 +0200

    add init-terraform

--- lisp/init-terraform.el
+++ lisp/init-terraform.el
@@ -1,12 +1,16 @@
 ;; By: Nick DeCoursin
 
-;(require-package 'terraform-mode) 
-;(require-package 'hcl-mode) 
+(require-package 'terraform-mode)
+(require-package 'hcl-mode)
 
 ;(custom-set-variables
 ; '(terraform-indent-level 4))
 
 ;(custom-set-variables
 ; '(hcl-indent-level 4))
+
+(require-package 'company-terraform)
+
+(company-terraform-init)
 
 (provide 'init-terraform)
