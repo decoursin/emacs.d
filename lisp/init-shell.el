392190e7765d47cf90abca921b8e90f28fabc6dc
Nick DeCoursin <ndecoursin@panono.com>
2019-12-05 14:39:03 +0100

    init bash shell repl (alias)

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -1,3 +1,10 @@
+;;
+;; TO start attached shell repl run: M-x shell (shell comamnd)
+;; or use the alias below
+;;
+
+(defalias 'bash-shell-repl 'shell)
+
 (setq sh-basic-offset 2)
 (setq sh-indentation 2)
 
